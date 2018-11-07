8290137b2aa67dfa204ef346d7264889d9cc364d
Kevin Gruber <kevin-gruber1@gmx.at>
2018-11-07 21:26:01 +0100

    created getters and setters, created 2 methods

--- ConstructorChallenge1/src/at/fhj/swd/BankAccount.java
+++ ConstructorChallenge1/src/at/fhj/swd/BankAccount.java
@@ -3,10 +3,73 @@ package at.fhj.swd;
 public class BankAccount {
 
     private int accountNumber;
-    private double balance;
+    private double accountBalance;
     private String customerName;
-    private String emailAdress;
+    private String emailAddress;
     private int phoneNumber;
 
+//    public BankAccount(int accountNumber, double balance, String customerName, String emailAdress, int phoneNumber) {
+//        this.accountNumber = accountNumber;
+//        this.accountBalance = balance;
+//        this.customerName = customerName;
+//        this.emailAddress = emailAdress;
+//        this.phoneNumber = phoneNumber;
+//    }
 
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void depositFunds(double depositAmount) {
+        this.accountBalance += depositAmount;
+        System.out.println("Deposit of " + depositAmount + " made. New balance is " + this.accountBalance);
+    }
+
+
+    public void withdrawFunds(double withdrawalAmount) {
+        if (this.accountBalance - withdrawalAmount < 0) {
+            System.out.println("Only " + this.accountBalance + " available. Withdrawal not processed!");
+        } else {
+            this.accountBalance -= withdrawalAmount;
+            System.out.println("Withdrawal of " + withdrawalAmount + " processed. Remaining balance = " + this.accountBalance);
+        }
+
+
+    }
 }
